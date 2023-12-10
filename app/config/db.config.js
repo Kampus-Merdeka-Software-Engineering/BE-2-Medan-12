0a6c2783af8d67c32807bcca33135590873eb7bf
Salman <salmannst15@gmail.com>
2023-12-11 04:33:04 +0700

    terbaru dotENV

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,17 +1,18 @@
-import {Sequelize} from 'sequelize';
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize('db_cospace','root','',{
-    HOST: '127.0.0.1',
-    USER: 'root',
-    PASSWORD: '',
-    DB: 'db_cospace',
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
 export { sequelize };
