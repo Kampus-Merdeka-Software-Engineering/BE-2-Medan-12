7b2967d91f8402d34fe633facb35aa2864889d02
Salman <salmannst15@gmail.com>
2023-12-12 00:18:25 +0700

    update DB CONFIG be

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express, { json, urlencoded } from 'express';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import userRoute from './app/routes/user.routes.js';
 import rsvRoute from './app/routes/reserve.routes.js';
 import rmRoute from './app/routes/room.routes.js';
@@ -9,16 +9,27 @@ import { sequelize } from './app/config/db.config.js';
 
 
 const app = express();
-const port = process.env.DB_PORT;
+const port = process.env.DB_PORT||process.env.PORT;
 
+dotenv.config();
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
 
-app.get('/', (req, res) => {
-    res.send('Server Online');
-});
+app.get('', (req, res) => {
+    res.send('Welcome')
+})
+
+app.get('/test-connection', async (req, res) => {
+    try {
+        await sequelize.authenticate()
+        console.log('Connection has been established successfully.')
+    } catch (e) {
+        console.error('Unable to connect to the database:', e)
+    }
+    res.send('Hello express')
+})
 
 app.use('/user', userRoute);
 app.use('/reserve', rsvRoute);
